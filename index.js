77a2cb2f868ce9575c6c0cae2f3dd46eb509295e
Merima Dedic <merimaxdedic@gmail.com>
2023-11-05 21:09:23 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+let random = Math.ceil(Math.random() * 100);
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max);
